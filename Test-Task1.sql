3d15d78e426a8fb51601531ea561a24a374eb11a
NehaSoni07 <37115137+NehaSoni07@users.noreply.github.com>
2019-02-25 19:10:54 +0530

    Add files via upload
    Test-Task1 : This file holds the query to create view and select the required data only.

---
+++ Test-Task1.sql
@@ -0,0 +1,22 @@
+DROP VIEW IF EXISTS vw_ingredient_curr_state;
+
+CREATE VIEW vw_ingredient_curr_state 
+AS 
+  SELECT id_ingredient, 
+         ingredient_name, 
+         price, 
+         time_stamp, 
+         deleted 
+  FROM   (SELECT id_ingredient, 
+                 ingredient_name, 
+                 price, 
+                 time_stamp, 
+                 deleted, 
+                 Row_number() 
+                   OVER( 
+                     PARTITION BY ingredient_name 
+                     ORDER BY time_stamp DESC) rtc 
+          FROM   ingredients 
+          WHERE  deleted = 0) temp 
+  WHERE  rtc = 1 
+  ORDER BY ingredient_name
